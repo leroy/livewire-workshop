0dd9309adedb7a8b289ccd111a60ac6706af0e03
Leroy Bakker <leroy@lorey.nl>
2024-03-20 09:35:09 +0100

    wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
     <title>Livewire Workshop</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
     @vite('resources/css/app.css')
 </head>
 <body class="bg-zinc-50 h-screen flex flex-col justify-center items-center">
